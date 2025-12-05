4c2a658cd60db8beb9656cd0df9f9c8c3434a76e
Dyl <152308287+Dylan-Mutabazi@users.noreply.github.com>
2025-12-05 12:43:45 -0500

    Initial commit

---
+++ code.java
@@ -0,0 +1,46 @@
+//imports
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Program topic
+ * 
+ *
+ * @author Dylan Mutabazi
+ * @version 1.0
+ * @since 2025-November
+ */
+final class Code {
+
+    /**
+     * Prevent instantiation of utility class.
+     *
+     * @exception IllegalStateException if constructor is called.
+     */
+    private Code() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    /**
+     * Sorts an array using the Bubble Sort algorithm.
+     *
+     * @param arr the array of integers to sort
+     * @return the sorted array
+     */
+    public static int[] fuctionName(final int[] arr) {
+        
+    }
+
+    /**
+     * Entrypoint of the program.
+     *
+     * @param args command-line arguments (unused)
+     */
+    public static void main(final String[] args) {
+        
+        }
+    }
+
